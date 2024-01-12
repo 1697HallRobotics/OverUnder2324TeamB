18bec24cc4f9415c81d00b17a595ba099f38b2d4
Hall Robotics WHPS <hallrobotics.whps@gmail.com>
2024-01-12 15:34:45 -0500

    pre-tournament commit

--- src/main2electricboogaloo.cpp
+++ src/main2electricboogaloo.cpp
@@ -4,22 +4,20 @@ brain Brain = brain(); // make sure the robot is not braindead
 competition Competition = competition();
 controller Controller = controller();
 
-motor frontLeftMotor = motor(PORT12, true);
-motor backLeftMotor = motor(PORT5, true);
+motor frontLeftMotor = motor(PORT11, true);
+motor backLeftMotor = motor(PORT1, true);
 
-motor frontRightMotor = motor(PORT11, false);
-motor backRightMotor = motor(PORT8, false);
+motor frontRightMotor = motor(PORT20, false);
+motor backRightMotor = motor(PORT10, false);
 
 motor_group leftMotors = motor_group(frontLeftMotor, backLeftMotor);
 motor_group rightMotors = motor_group(frontRightMotor, backRightMotor);
 
-motor cataMotorL = motor(PORT10, true);
-motor cataMotorR = motor(PORT6, false);
-motor_group cataMotor = motor_group(cataMotorL, cataMotorR);
+motor cataMotor = motor(PORT9, true);
 
-motor intakeMotor = motor(PORT2, true);
+motor intakeMotor = motor(PORT12, true);
 
-pneumatics Pneumatics = pneumatics(Brain.ThreeWirePort.A);
+motor wingMotor = motor(PORT2, false);
 
 bool spinning = false;
 
@@ -39,26 +37,46 @@ void pre_auton() {
 
     cataMotor.setMaxTorque(100, pct);
     cataMotor.setVelocity(75, pct);
-
     leftMotors.setVelocity(100, pct);
     rightMotors.setVelocity(100, pct);
+
+    wingMotor.setVelocity(100, pct);
+    wingMotor.setMaxTorque(100, pct);
 
     Brain.Screen.drawImageFromBuffer(logo_blue_map, 0, 0, sizeof(logo_blue_map));
 }
 
-void autonomous_red() {
-    leftMotors.spinFor(reverse, 800, deg, false);
-    rightMotors.spinFor(reverse, 800, deg, false);
-    this_thread::sleep_for(2 * 1000);
-    leftMotors.spinFor(fwd, 200, deg, false);
-    rightMotors.spinFor(fwd, 200, deg, false);
+void autonomous_atk() {
+    cataMotor.spinFor(fwd, 700, deg, false);
+    
+    leftMotors.spinFor(fwd, 800, deg, false);
+    rightMotors.spinFor(fwd, 800, deg, false);
+    this_thread::sleep_for(1.5 * 1000);
+    leftMotors.spinFor(reverse, 200, deg, false);
+    rightMotors.spinFor(reverse, 200, deg, false);
+    
 }
-void autonomous_blu() {
+void autonomous_def() {
     intakeMotor.spinFor(fwd, 5, sec);
 }
 
+bool blockingR2Function = false;
+bool wingsOut = false;
+
 void driver_control() {
-    Controller.ButtonA.pressed(spinCatapult);
+    Controller.ButtonR1.pressed([]() {
+        blockingR2Function = true;
+        cataMotor.spinFor(fwd, 720, deg, true);
+        blockingR2Function = false;
+    });
+    Controller.ButtonA.pressed([]() {
+        if (wingsOut) {
+            wingMotor.spinFor(reverse, 190, deg, false);
+        } else {
+            wingMotor.spinFor(fwd, 190, deg, std::false_type::value);
+        }
+        wingsOut = !wingsOut;
+    });
 
     cataMotor.setPosition(0, deg);
 
@@ -86,20 +104,18 @@ void driver_control() {
             intakeMotor.stop(hold);
         }
 
-        if (Controller.ButtonR2.pressing()) {
+        if (Controller.ButtonR2.pressing() && !blockingR2Function) {
             cataMotor.spin(fwd);
-        } else {
+        } else if (!blockingR2Function) {
             cataMotor.stop(hold);
         }
-        
     }
 }
 
 int main() {
-    Competition.autonomous(autonomous_red);
+    Competition.autonomous(autonomous_atk);
     Competition.drivercontrol(driver_control);
     
-
     // Run the pre-autonomous function. 
     pre_auton();
 
